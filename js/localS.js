1ddc4303dba70a55564095bb2cda8c7f72fc6160
Marius <marius@example.com>
2022-03-18 08:51:15 +0200

    uzd2

---
+++ js/localS.js
@@ -0,0 +1,15 @@
+/* 1
+<button id="btn1">Click me</button>
+    <h2>0</h2>
+    padaryti kad spaudzian mygtuka didinam 0 vienetu
+*/
+
+/* 2
+<form>
+  <input type="text" id="name" placeholder="Enter name" />
+</form>
+
+ivedus varda ir issiuntus forma su enter(automatiskai vysksta)
+pridedam ta varda i vardu sarasa, kaip saraso el
+
+*/
