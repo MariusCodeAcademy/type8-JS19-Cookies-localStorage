9bac3bfd87b619353d3ff2d3e6b3d7118dde6846
Marius <marius@example.com>
2022-03-18 10:02:59 +0200

    local s

---
+++ js/localIntro.js
@@ -0,0 +1,10 @@
+// issaugoti reiksme
+localStorage.setItem('manoVardas', 'Marius');
+localStorage.setItem('loginId', '125');
+
+// pasiimti reiksmes
+const vard = localStorage.getItem('manoVardas');
+console.log('vard ===', vard);
+
+const kitaR = localStorage.getItem('manoVardas11');
+console.log('kitaR ===', kitaR);
--- js/localS.js
+++ js/localS.js
@@ -5,9 +5,11 @@
 */
 const btnEl = document.getElementById('btn1');
 const h2El = btnEl.nextElementSibling;
+// is storage
 let total = 0;
 btnEl.onclick = () => {
   h2El.textContent = ++total;
+  // i storage
 };
 
 /* 2
@@ -19,3 +21,25 @@ ivedus varda ir issiuntus forma su enter(automatiskai vysksta)
 pridedam ta varda i vardu sarasa, kaip saraso el
 
 */
+const formEl = document.forms[0];
+const ulEl = document.querySelector('.names');
+
+formEl.onsubmit = function (e) {
+  e.preventDefault();
+  const inpEl = formEl.elements.name;
+  //paimti reiksme
+  let inputVal = inpEl.value.trim();
+  console.log('inputVal ===', typeof inputVal);
+  addLi(inputVal);
+  // inputVal = ''; // neveikia nes isvalom reiksme o ne el
+  inpEl.value = '';
+};
+
+function addLi(stringName) {
+  // create li
+  const liEl = document.createElement('li');
+  // add text
+  liEl.textContent = stringName;
+  // place el
+  ulEl.append(liEl);
+}
